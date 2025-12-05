7bfaa67b4b72d523bf3aa82cf9ab53531ebc3804
YuxianDu <yuxiandu5@gmail.com>
2025-12-06 02:39:35 +1100

    first commit

---
+++ src/HomePage.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Button from './components/ui/button'
+
+function HomePage() {
+  return (
+    <div className='flex justify-center items-center text-emerald-700 h-screen w-screen '>
+      Hello World
+      <Button>Click</Button>
+    </div>
+  )
+}
+
+export default HomePage
---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import HomePage from './HomePage.tsx'
+import { BrowserRouter, Routes, Route } from 'react-router'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<HomePage />}/>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>,
+)
