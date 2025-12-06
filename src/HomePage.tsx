9a3d299f06a9ba93555105f9cae12564844e67ad
YuxianDu <yuxiandu5@gmail.com>
2025-12-06 15:20:14 +1100

    chore: initialize config

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,11 +1,13 @@
 import './App.css'
-import Button from './components/ui/button'
+import { Button } from './components/ui/Button'
 
 function HomePage() {
   return (
-    <div className='flex justify-center items-center text-emerald-700 h-screen w-screen '>
+    <div className="flex h-screen w-screen items-center justify-center text-emerald-700">
       Hello World
-      <Button>Click</Button>
+      <Button variant={'ghost'} onClick={() => console.log('reached')}>
+        Click me
+      </Button>
     </div>
   )
 }
