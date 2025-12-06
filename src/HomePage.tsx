44ecdd34028193b184ff7e6788673baae8a56925
YuxianDu <yuxiandu5@gmail.com>
2025-12-06 15:43:10 +1100

    chore: fix import path

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,6 +1,6 @@
 import type { FC } from 'react'
 import './App.css'
-import Button from './components/ui/Button'
+import Button from './components/ui/Button.tsx'
 
 const HomePage: FC = () => (
   <div className="flex h-screen w-screen items-center justify-center text-emerald-700">
